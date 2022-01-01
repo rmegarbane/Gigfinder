2e11cefa6eeba22aa84db5be296fffb96fd05492
Ashton Bunn <ashtonwork@hotmail.com>
2022-01-01 22:31:42 +0800

    Added some features to index

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,64 +1,67 @@
-
-const app = getApp()
-
+// pages/index/index.js
 Page({
+
+  /**
+   * Page initial data
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
-  },
- 
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
+
   },
 
-  getUserProfile(e) {
-    console.log("clicked info", e)
-    let page = this
-    wx.getUserProfile({
-      desc: 'got user profile',
-      success: (res) => {
-        console.log("error", app.globalData)
-        const userId = app.globalData.user.id
-        // update part in backend
-        wx.request({
-          url: `${app.globalData.url}/users/${userId}`,
-          method: 'PUT', // or PATCH 
-          data: {
-            userInfo: res.userInfo
-          },
-          success: (res) => {
-            page.setData({
-              user: res.data.currentUser,
-              hasUserInfo: true
-            })
-          }
-        })
-        // this.setData({
-        //   userInfo: res.userInfo,
-        //   hasUserInfo: true
-        // })
-      }
-    })
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
   },
 
-  getUserInfo(e) {
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  },
+  
 })
