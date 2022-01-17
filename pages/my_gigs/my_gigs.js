d7297db64a5428da881fd27764929df8830cf71a
Adam Matteson <matteson211@hotmail.com>
2022-01-17 23:08:34 +0800

    no progress:

--- pages/my_gigs/my_gigs.js
+++ pages/my_gigs/my_gigs.js
@@ -15,16 +15,63 @@ Page({
     })
   },
   
+  // onLoad: function (options) {
+  //   const page = this
+  //   wx.request({
+  //     header: wx.getStorageSync('headers'),
+  //     url: `${app.globalData.url}`,
+  //     success: res => {
+  //       console.log(res)
+  //       page.setData(res.data)
+  //     }
+  //   })
+  // },
+
+
   onLoad: function (options) {
     const page = this
+    const user = wx.getStorageSync('user')
+    page.setData({user})
     wx.request({
       header: wx.getStorageSync('headers'),
-      url: `${app.globalData.url}`,
+      // url: `http://localhost:3000/api/v1/gigs/${options.id}`,
+      url: `${app.globalData.url}/gigs/${options.id}`,
       success: res => {
         console.log(res)
-        page.setData(res.data)
+        page.setData({gig: res.data})
+        wx.setNavigationBarTitle({
+          title: page.data.gig.title,
+        })
       }
     })
+
+    // const id = options.id
+    // const app = getApp()
+    // const globalData = app.globalData
+    // this.setData(globalData)
+    // const gigs = globalData.gigs
+
+    // let gig = gigs.filter(gig => gig.id == id)
+
+    // const page = this
+    // page.setData(gig[0])
+
+
+    // const page = this
+    // const gig_id = options.id
+    // const auth = wx.getStorageSync('auth')
+    // const header = {
+    //   'X-User-Email': auth.email,
+    //   'X-User-Token': auth.token
+    // }
+    // console.log("This is the header", header)
+    // wx.request({
+    //   url: `${getApp().globalData.baseUrl}/gigs/${gig_id}`,
+    //   header, 
+    //   success(res) {
+    //     page.setData({ gig: res.data.gig })
+    //   }
+    // })
   },
 
 
